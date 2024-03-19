605a0d458aa4ca71b993ce666e030aa0b302b5b7
João Guilherme <joao.sczip@hotmail.com>
2024-03-18 21:29:53 -0300

    return the weather when it is found

---
+++ internal/handlers/temperature.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"context"
+	"time"
+
+	"github.com/joaosczip/zipcode_temp/pkg/weather"
+	"github.com/joaosczip/zipcode_temp/pkg/zipcode"
+)
+
+type TemperatureHandler struct {
+	zipCodeClient zipcode.ZipCodeClient
+	weatherClient weather.WeatherClient
+}
+
+func NewTemperatureHandler(zipCodeClient zipcode.ZipCodeClient, weatherClient weather.WeatherClient) *TemperatureHandler {
+	return &TemperatureHandler{zipCodeClient: zipCodeClient, weatherClient: weatherClient}
+}
+
+type FetchTemperatureOutput struct {
+	Celsius    float64 `json:"celsius"`
+	Fahrenheit float64 `json:"fahrenheit"`
+	Kelvin     float64 `json:"kelvin"`
+}
+
+func (h *TemperatureHandler) FetchTemperature(zipCode string) (FetchTemperatureOutput, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	city, err := h.zipCodeClient.Fetch(ctx, zipCode)
+
+	if err != nil {
+		return FetchTemperatureOutput{}, err
+	}
+
+	weather, err := h.weatherClient.Fetch(ctx, city.Name)
+
+	if err != nil {
+		return FetchTemperatureOutput{}, err
+	}
+
+	return FetchTemperatureOutput{
+		Celsius:    weather.Celsius,
+		Fahrenheit: weather.Fahrenheit,
+		Kelvin:     weather.Kelvin,
+	}, nil
+}
---
+++ internal/handlers/temperature_test.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"context"
+	"testing"
+
+	"github.com/joaosczip/zipcode_temp/pkg/weather"
+	"github.com/joaosczip/zipcode_temp/pkg/zipcode"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type ZipCodeClientMock struct {
+	mock.Mock
+}
+
+func (m *ZipCodeClientMock) Fetch(ctx context.Context, zipCode string) (*zipcode.City, error) {
+	args := m.Called(zipCode)
+	return args.Get(0).(*zipcode.City), args.Error(1)
+}
+
+type WeatherClientMock struct {
+	mock.Mock
+}
+
+func (m *WeatherClientMock) Fetch(ctx context.Context, city string) (*weather.Weather, error) {
+	args := m.Called(city)
+	return args.Get(0).(*weather.Weather), args.Error(1)
+}
+
+func TestTemperatureHandler_FetchTemperature(t *testing.T) {
+	t.Run("should return the temperature when the zip code is found", func(t *testing.T) {
+		zipCodeClientMock := &ZipCodeClientMock{}
+		weatherClientMock := &WeatherClientMock{}
+
+		zipCodeClientMock.On("Fetch", "01001000").Return(&zipcode.City{
+			Name: "São Paulo",
+		}, nil)
+
+		weatherClientMock.On("Fetch", "São Paulo").Return(&weather.Weather{
+			Celsius:    22.0,
+			Fahrenheit: 71.6,
+			Kelvin:     295.0,
+		}, nil)
+
+		handler := NewTemperatureHandler(zipCodeClientMock, weatherClientMock)
+
+		output, err := handler.FetchTemperature("01001000")
+
+		assert.Nil(t, err)
+		assert.Equal(t, output.Celsius, 22.0)
+		assert.Equal(t, output.Fahrenheit, 71.6)
+		assert.Equal(t, output.Kelvin, 295.0)
+
+		zipCodeClientMock.AssertExpectations(t)
+		weatherClientMock.AssertExpectations(t)
+	})
+}
